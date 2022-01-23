b2b27b23ee5a26764d8b4105cd4d5a84de258c1d
Abdullah <Abdullahadal8.aa@gmail.com>
2022-01-24 01:18:51 +0300

    FlutterDegiskenler

---
+++ android/app/src/main/kotlin/com/example/degiskenler_ve_veri_tipleri/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.degiskenler_ve_veri_tipleri
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/DegiskenOlusturma.dart
@@ -0,0 +1,28 @@
+void main() {
+
+  var ogrenciAdi      = "Abdullah";
+  var ogrenciyasi     = 22;
+  var ogrenciBoyu     = 1.77;
+  var ogrenciBasHarfi = "A";
+
+  print(ogrenciAdi);
+  print(ogrenciyasi);
+  print(ogrenciBoyu);
+  print(ogrenciBasHarfi);
+
+
+  //Proje klasorünün üstüne sağ tıklayıp RUN diyerek sonucu görebiliriz;
+
+  int     urun_id         =  2525;
+  String  urun_adi        =  "Bilgisayar Kasası";
+  int     urun_adet       =  1000;
+  double  urun_fiyat      =  349.99;
+  String  urun_tedarikci  =  "MSI";
+
+  print("Ürün id        : $urun_id");
+  print("Ürün adi       : $urun_adi");
+  print("Ürün adet      : $urun_adet");
+  print("Ürün fiyat     : $urun_fiyat");
+  print("Ürün tedarikçi : $urun_tedarikci");
+
+}
